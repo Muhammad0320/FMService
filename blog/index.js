fb69efeb5e1016cbdd6f1645a20604d8121ac357
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-03 04:49:54 +0100

    chore: add all routes for blog serveice

--- blog/index.js
+++ blog/index.js
@@ -4,7 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const crypto_1 = require("crypto");
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 console.log("Hi mom");
 app.use(body_parser_1.default.json());
+const posts = {};
+app.get("/posts", (req, res) => {
+    res.send(posts);
+});
+app.post("/posts", () => {
+    const id = (0, crypto_1.randomBytes)(4).toString("hex");
+    const { title } = req.body;
+});
--- blog/index.ts
+++ blog/index.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { randomBytes } from "crypto";
 
 import bodyParser from "body-parser";
 
@@ -7,3 +8,15 @@ const app = express();
 console.log("Hi mom");
 
 app.use(bodyParser.json());
+
+const posts = {};
+
+app.get("/posts", (req: Request, res: Response) => {
+  res.send(posts);
+});
+
+app.post("/posts", () => {
+  const id = randomBytes(4).toString("hex");
+
+  const { title } = req.body;
+});
