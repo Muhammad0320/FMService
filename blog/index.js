c9b14460d0afa90619840af552629a856e0091dd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-03 04:54:01 +0100

    chore: declared needed types

--- blog/index.js
+++ blog/index.js
@@ -13,7 +13,12 @@ const posts = {};
 app.get("/posts", (req, res) => {
     res.send(posts);
 });
-app.post("/posts", () => {
+app.post("/posts", (req, res) => {
     const id = (0, crypto_1.randomBytes)(4).toString("hex");
     const { title } = req.body;
+    posts[id] = {
+        id,
+        title,
+    };
+    res.status(201).send();
 });
--- blog/index.ts
+++ blog/index.ts
@@ -9,14 +9,30 @@ console.log("Hi mom");
 
 app.use(bodyParser.json());
 
+interface Posts {
+  id: string;
+  title: string;
+}
+
+interface ReqWithBoody extends Request {
+  body?: { [key: string]: string };
+}
+
 const posts = {};
 
 app.get("/posts", (req: Request, res: Response) => {
   res.send(posts);
 });
 
-app.post("/posts", () => {
+app.post("/posts", (req: Request, res: Response) => {
   const id = randomBytes(4).toString("hex");
 
   const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send();
 });
