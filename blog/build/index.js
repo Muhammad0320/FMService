281a32b5e9aa469f0522d2fd98c174cd5911a30a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-03 05:37:36 +0100

    chore: update fs

---
+++ blog/build/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const crypto_1 = require("crypto");
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+console.log("Hi mom");
+app.use(body_parser_1.default.json());
+const posts = {};
+app.get("/posts", (req, res) => {
+    res.send(posts);
+});
+app.post("/posts", (req, res) => {
+    const id = (0, crypto_1.randomBytes)(4).toString("hex");
+    const { content } = req.body;
+    posts[id] = {
+        id,
+        content,
+    };
+    res.status(201).send(posts[id]);
+});
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
