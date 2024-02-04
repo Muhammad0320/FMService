64ed1a723de6bdaf823050d16523bf44ff77e3cc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-04 07:46:17 +0100

    chore: add post route to listen to req.body and send response to all services

--- event-bus/src/index.ts
+++ event-bus/src/index.ts
@@ -1,8 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 
 console.log("Hi mom");
 
 app.use(bodyParser.json());
+
+app.post("/events", async (req: Request, res: Response) => {
+  const event = req.body;
+
+  await axios.post("http://localhost:4000/event", event);
+  await axios.post("http://localhost:4001/event", event);
+  await axios.post("http://localhost:4002/event", event);
+});
+
+const port = 4005;
+
+app.listen(port, () => {
+  console.log(`listening to port  ${port} `);
+});
