5c923cc295b7e3190328a7055150e24645bf69f0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-05 06:40:51 +0100

    chore: update index js file

--- query/src/index.ts
+++ query/src/index.ts
@@ -1,1 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
 console.log("Hi mom");
+
+app.get("/posts", (req: Request, res: Response) => {});
+
+app.post("/events", (req: Request, res: Response) => {});
+
+const port = 4002;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port} `);
+});
