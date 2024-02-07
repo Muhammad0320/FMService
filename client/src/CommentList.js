5d922ea5526833ad2c8df07f060964ceb91b8b9e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-07 05:39:56 +0100

    chore: update comment list

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,17 +2,18 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments?.map((comment) => {
-    return <li key={comment?.id}>{comment?.content}</li>;
+    let commentText;
+
+    if (comment.status === "approved") commentText = comment.content;
+
+    if (comment.status === "pending")
+      commentText = "This comment is awaiting moderation";
+
+    if (comment.status === "rejected")
+      commentText = "This comment was rejected";
+
+    return <li key={comment?.id}>{commentText}</li>;
   });
-
-  let comment;
-
-  if (comments.status === "approved") comment = comments.status;
-
-  if (comment.status === "pending")
-    comment = "This comment is awaiting moderation";
-
-  if (comment.status === "rejected") comment = "This comment was rejected";
 
   return <ul>{renderedComments}</ul>;
   // return <div> okay </div>;
